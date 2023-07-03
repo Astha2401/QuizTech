c53e13eb63238bc3343db39386a20585acf6f9b0
Astha bhushan <114616996+Astha2401@users.noreply.github.com>
2023-07-03 15:01:50 +0530

    Delete quiza directory

--- quiza/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// Function to display a confirmation message before submitting a form
-function confirmFormSubmission() {
-    return confirm("Are you sure you want to submit the form?");
-}
-
-// Function to display a confirmation message before logging out
-function confirmLogout() {
-    return confirm("Are you sure you want to log out?");
-}
